4a2e636909be4feebb8e8a08b9398dd1b76e4e3f
Sebastian Freundt <freundt@math.tu-berlin.de>
2008-12-26 20:14:43 +0000

    Summary: minor, use a stack of global contexts each with its own buffer Keywords:
    Signed-off-by: Sebastian Freundt <freundt@math.tu-berlin.de>

--- src/unserding.c
+++ src/unserding.c
@@ -63,21 +63,184 @@
 	fprintf(stderr, "[unserding/input/tcpudp] CRITICAL " args)
 #define INPUT_DEBUG_TCPUDP(args...)			\
 	fprintf(stderr, "[unserding/input/tcpudp] " args)
-#define UNLIKELY
+#define TCPUDP_TIMEOUT		5
 
-ev_io tcpudp_watcher;
-ev_timer timeout_watcher;
-ev_idle idle_watcher;
+#if !defined LIKELY
+# define LIKELY(_x)	__builtin_expect((_x), 1)
+#endif
+#if !defined UNLIKELY
+# define UNLIKELY(_x)	__builtin_expect((_x), 0)
+#endif
 
-struct addrinfo glob_sa;
+#define countof(x)		(sizeof(x) / sizeof(*x))
+
+/* The encoded parameter sizes will be rounded up to match pointer alignment. */
+#define ROUND(s, a)		(a * ((s + a - 1) / a))
+#define aligned_sizeof(t)	ROUND(sizeof(t), __alignof(void*))
+
+typedef size_t index_t;
+typedef struct conn_ctx_s *conn_ctx_t;
+typedef struct outbuf_s *outbuf_t;
+
+struct conn_ctx_s {
+	struct ev_io __attribute__((aligned(16))) io;
+	struct ev_timer __attribute__((aligned(16))) ti;
+	int src;
+	int snk;
+	index_t bidx;
+	char buf[1024 - 32 - sizeof(struct ev_timer) - sizeof(struct ev_io)]
+	__attribute__((aligned(16)));
+} __attribute__((aligned(1024)));
+
+struct outbuf_s {
+	struct ev_io __attribute__((aligned(16))) io;
+	size_t buflen;
+	const char *buffer;
+};
+
+static inline conn_ctx_t __attribute__((always_inline, gnu_inline))
+ev_io_ctx(void *io)
+{
+	return (void*)io;
+}
+
+static inline conn_ctx_t __attribute__((always_inline, gnu_inline))
+ev_timer_ctx(void *timer)
+{
+	return (void*)((char*)timer - aligned_sizeof(struct ev_io));
+}
+
+static inline void __attribute__((always_inline, gnu_inline)) *
+ctx_io(conn_ctx_t ctx)
+{
+	return (void*)&ctx->io;
+}
+
+static inline void __attribute__((always_inline, gnu_inline)) *
+ctx_timer(conn_ctx_t ctx)
+{
+	return (void*)&ctx->ti;
+}
+
+static index_t __attribute__((unused)) glob_idx = 0;
+static struct conn_ctx_s glob_ctx[64];
+static struct addrinfo glob_sa;
+static struct sockaddr_in glob_mul;
+
+static ev_io __srv_watcher __attribute__((aligned(16)));
+static ev_io __srvmul_watcher __attribute__((aligned(16)));
+static ev_signal __sigint_watcher __attribute__((aligned(16)));
+static ev_signal __sigpipe_watcher __attribute__((aligned(16)));
+
+/* protos */
+static void tcpudp_listener_deinit(int sock);
 
 
+static inline void __attribute__((always_inline))
+tcpudp_kick_ctx(EV_P_ conn_ctx_t ctx)
+{
+	ev_io *evio = ctx_io(ctx);
+	ev_timer *evti = ctx_timer(ctx);
+
+	/* kick the timer */
+	ev_timer_stop(EV_A_ evti);
+	/* kick the io handler */
+	ev_io_stop(EV_A_ evio);
+	/* stop the bugger */
+	tcpudp_listener_deinit(ctx->snk);
+	/* finally, give the ctx struct a proper rinse */
+	ctx->src = ctx->snk = -1;
+	ctx->bidx = 0;
+	return;
+}
+
+static inline void
+tcpudp_ctx_renew_timer(EV_P_ conn_ctx_t ctx)
+{
+	ev_timer *evti = ctx_timer(ctx);
+
+	ev_timer_stop(EV_A_ evti);
+	ev_timer_set(evti, TCPUDP_TIMEOUT, 0.0);
+	ev_timer_start(EV_A_ evti);
+	return;
+}
+
 /* this callback is called when data is readable on one of the polled socks */
 static void
 tcpudp_traf_rcb(EV_P_ ev_io *w, int revents)
 {
+	size_t nread;
+	/* our brilliant type pun */
+	struct conn_ctx_s *ctx = (void*)w;
+
 	INPUT_DEBUG_TCPUDP("traffic on %d\n", w->fd);
+	nread = read(w->fd, &ctx->buf[ctx->bidx],
+		     countof(ctx->buf) - ctx->bidx);
+	if (UNLIKELY(nread == 0)) {
+		tcpudp_kick_ctx(EV_A_ ctx);
+		return;
+	}
+	/* prolongate the timer a wee bit */
+	tcpudp_ctx_renew_timer(EV_A_ ctx);
+	/* wind the buffer index */
+	ctx->bidx += nread;
 	return;
+}
+
+/* this callback is called when data is writable on one of the polled socks */
+static void __attribute__((unused))
+tcpudp_traf_wcb(EV_P_ ev_io *w, int revents)
+{
+	outbuf_t ob = (void*)w;
+	write(w->fd, ob->buffer, ob->buflen);
+	ev_io_stop(EV_A_ w);
+	return;
+}
+
+static const char idle_msg[] = "Oi shitface, stop wasting my time, will ya!\n";
+static const char emer_msg[] = "unserding has been shut down, cya mate!\n";
+
+static void
+tcpudp_idleto_cb(EV_P_ ev_timer *w, int revents)
+{
+	/* our brilliant type pun */
+	struct conn_ctx_s *ctx = ev_timer_ctx(w);
+
+	INPUT_DEBUG_TCPUDP("bitching back at the eejit on %d\n", ctx->snk);
+	write(ctx->snk, idle_msg, countof(idle_msg));
+
+	/* just finish him off */
+	tcpudp_kick_ctx(EV_A_ ctx);
+	return;
+}
+
+static void
+sigint_cb(EV_P_ ev_signal *w, int revents)
+{
+	INPUT_DEBUG_TCPUDP("C-c caught, unrolling everything\n");
+	/* kill all open connexions */
+	for (index_t res = 0; res < countof(glob_ctx); res++) {
+		if (LIKELY(glob_ctx[res].snk == -1)) {
+			continue;
+		}
+		INPUT_DEBUG_TCPUDP("letting %d know\n", glob_ctx[res].snk);
+		write(glob_ctx[res].snk, emer_msg, countof(emer_msg));
+		tcpudp_kick_ctx(EV_A_ &glob_ctx[res]);
+	}
+	ev_unloop(EV_A_ EVUNLOOP_ALL);
+	return;
+}
+
+static inline index_t
+find_ctx(void)
+{
+/* returns the next free context */
+	for (index_t res = 0; res < countof(glob_ctx); res++) {
+		if (glob_ctx[res].snk == -1) {
+			return res;
+		}
+	}
+	return -1;
 }
 
 /* this callback is called when data is readable on the main server socket */
@@ -92,7 +255,9 @@ tcpudp_inco_cb(EV_P_ ev_io *w, int revents)
 	const char *a;
 	/* the port (in host-byte order) */
 	uint16_t p;
+	index_t widx;
 	ev_io *watcher;
+	ev_timer *to;
 
 	INPUT_DEBUG_TCPUDP("incoming connection\n");
 
@@ -108,13 +273,25 @@ tcpudp_inco_cb(EV_P_ ev_io *w, int revents)
 	INPUT_DEBUG_TCPUDP("Server: connect from host %s, port %d.\n", a, p);
 
 	/* initialise an io watcher, then start it */
-	watcher = malloc(sizeof(ev_io));
+	widx = find_ctx();
+
+	/* escrow the socket poller */
+	watcher = ctx_io(&glob_ctx[widx]);
 	ev_io_init(watcher, tcpudp_traf_rcb, ns, EV_READ);
-	ev_io_start(loop, watcher);
+	ev_io_start(EV_A_ watcher);
+
+	/* escrow the connexion idle timeout */
+	to = ctx_timer(&glob_ctx[widx]);
+	ev_timer_init(to, tcpudp_idleto_cb, TCPUDP_TIMEOUT, 0.);
+	ev_timer_start(EV_A_ to);
+
+	/* put the src and snk sock into glob_ctx */
+	glob_ctx[widx].src = w->fd;
+	glob_ctx[widx].snk = ns;
 	return;
 }
 
-static void
+static void __attribute__((unused))
 timeout_cb(EV_P_ ev_timer *w, int revents)
 {
 	puts("timeout");
@@ -240,6 +417,62 @@ tcpudp_listener_init(void)
 	return s;
 }
 
+#if 0
+static int
+tcpudp_mlistener_init(void)
+{
+	struct addrinfo *res;
+	const struct addrinfo hints = {
+		.ai_family = AF_INET6,
+		.ai_socktype = SOCK_DGRAM,
+		.ai_protocol = IPPROTO_UDP,
+		/* specify to whom we listen */
+		.ai_flags = AI_PASSIVE | AI_V4MAPPED | AI_ALL,
+	};
+	int retval;
+	volatile int s;
+	struct sockaddr_in saddr;
+	struct ip_mreq imreq;
+
+	/* set content of struct saddr and imreq to zero */
+	memset(&saddr, 0, sizeof(struct sockaddr_in));
+	memset(&imreq, 0, sizeof(struct ip_mreq));
+
+	/* open a UDP socket */
+	s = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+	if (s < 0) {
+		INPUT_CRITICAL_TCPUDP("Unable to open multicast socket\n");
+		return -1;
+	}
+
+	saddr.sin_family = PF_INET;
+	saddr.sin_port = htons(8653);
+	saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	retval = bind(s, (struct sockaddr*)&saddr, sizeof(struct sockaddr_in));
+
+	if (retval < 1) {
+		INPUT_CRITICAL_TCPUDP("Unable to bind multicast socket\n");
+		return -1;
+	}
+
+	imreq.imr_multiaddr.s_addr = inet_addr("224.0.0.1");
+	imreq.imr_interface.s_addr = INADDR_ANY;
+
+	/* set TTL of multicast packet */
+	retval = setsockopt(s,
+			    saddr.ai_family == PF_INET6 ? IPPROTO_IPV6 : IPPROTO_IP,
+			    saddr.ai_family == PF_INET6 ? IPV6_MULTICAST_HOPS : IP_MULTICAST_TTL,
+			    (char*) &multicastTTL, sizeof(multicastTTL));
+
+	if (retval != 0) {
+		abort();
+	}
+
+	/* succeeded if > 0 */
+	return s;
+}
+#endif
+
 static void
 tcpudp_listener_deinit(int sock)
 {
@@ -252,30 +485,50 @@ tcpudp_listener_deinit(int sock)
 }
 
 
+static void
+init_glob_ctx(void)
+{
+	for (index_t i = 0; i < countof(glob_ctx); i++) {
+		glob_ctx[i].snk = -1;
+	}
+	return;
+}
+
 int
 main (void)
 {
 	/* use the default event loop unless you have special needs */
 	struct ev_loop *loop = ev_default_loop(0);
 	int lsock = tcpudp_listener_init();
+#if 0
+	int msock = tcpudp_mlistener_init();
+#endif
+	ev_io *srv_watcher = &__srv_watcher;
+	ev_io *srvmul_watcher = &__srvmul_watcher;
+	ev_signal *sigint_watcher = &__sigint_watcher;
+	ev_signal *sigpipe_watcher = &__sigpipe_watcher;
+
+	/* initialise the global context */
+	init_glob_ctx();
 
 	/* initialise an io watcher, then start it */
-	ev_io_init(&tcpudp_watcher, tcpudp_inco_cb, lsock, EV_READ);
-	ev_io_start(loop, &tcpudp_watcher);
-
-	/* initialise a timer watcher, then start it
-	 * simple non-repeating 15.5 second timeout */
-	ev_timer_init(&timeout_watcher, timeout_cb, 15.5, 0.);
-	ev_timer_start(loop, &timeout_watcher);
-
+	ev_io_init(srv_watcher, tcpudp_inco_cb, lsock, EV_READ);
+	ev_io_start(EV_A_ srv_watcher);
 #if 0
-	/* initialise an idler */
-	ev_idle_init(&idle_watcher, idle_cb);
-	ev_idle_start(loop, &idle_watcher);
+	/* initialise a multicast watcher */
+	ev_io_init(srvmul_watcher, tcpudp_inco_cb, msock, EV_READ);
+	ev_io_start(EV_A_ srvmul_watcher);
 #endif
 
+	/* initialise a sig C-c handler */
+	ev_signal_init(sigint_watcher, sigint_cb, SIGINT);
+	ev_signal_start(EV_A_ sigint_watcher);
+	/* initialise a sig C-c handler */
+	ev_signal_init(sigpipe_watcher, sigint_cb, SIGPIPE);
+	ev_signal_start(EV_A_ sigpipe_watcher);
+
 	/* now wait for events to arrive */
-	ev_loop(loop, 0);
+	ev_loop(EV_A_ 0);
 
 	/* close the socket */
 	tcpudp_listener_deinit(lsock);

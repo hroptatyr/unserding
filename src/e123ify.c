50a1ce7fa9c7fd05ba675feb802480fda9e4c458
Sebastian Freundt <freundt@math.tu-berlin.de>
2009-03-23 23:50:27 +0000

    Summary: minor, poptify e123ify Keywords: service e.123
    Signed-off-by: Sebastian Freundt <freundt@math.tu-berlin.de>

--- src/e123ify.c
+++ src/e123ify.c
@@ -39,8 +39,19 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
+#define HAVE_POPT
+#include <popt.h>
+
 #include "unserding.h"
 #include "protocore.h"
+
+/* could be used like so:
+ * grep -o -e \
+ *   "\"\\(\\+\\|(\\|\\[\\)[[:digit:]]\
+ *   [- .()[:digit:]]\\{6,\\}[- /.,()[:alnum:]]*\"" \
+ *   <infile> | e123ify -s */
+
+static int sed_mode = 0;
 
 #define TIMEOUT		100 /* milliseconds */
 
@@ -126,11 +137,12 @@ e123ify_send(ud_handle_t hdl, const char *number)
 	return cno;
 }
 
+static char buf[UDPC_SIMPLE_PKTLEN];
+static char *output = &buf[10];
+
 static int
 e123ify_recv(ud_handle_t hdl, ud_convo_t cno)
 {
-	char buf[UDPC_SIMPLE_PKTLEN];
-	char *work_space;
 	ud_packet_t pkt = {sizeof(buf), buf};
 	void *clo = (void*)(long unsigned int)cno;
 
@@ -139,30 +151,46 @@ e123ify_recv(ud_handle_t hdl, ud_convo_t cno)
 	ud_recv_pred(hdl, &pkt, TIMEOUT, usefulp, clo);
 
 	if (!usefulp(pkt, clo)) {
-		fprintf(stderr, "No answers\n");
 		return 1;
 	}
 	/* prepare the buffer for string output */
-	work_space = &pkt.pbuf[10];
-	work_space[pkt.pbuf[9]] = '\0';
-	/* out him */
-	fputs(work_space, stdout);
-	fputc('\n', stdout);
+	output[pkt.pbuf[9]] = '\0';
 	return 0;
+}
+
+static void
+out_him(void)
+{
+	/* out him */
+	fputs(output, stdout);
+	fputc('\n', stdout);
+}
+
+static void
+__e123ify(ud_handle_t hdl, const char *arg)
+{
+	ud_convo_t cno;	
+
+	/* query the bugger */
+	cno = e123ify_send(hdl, arg);
+	/* receive */
+	if (!e123ify_recv(hdl, cno)) {
+		out_him();
+	} else {
+		fprintf(stderr, "No answers\n");
+	}
+	return;
 }
 
 
 /* modes */
 static void
-cli_mode(ud_handle_t hdl, int argc, const char *argv[])
+cli_mode(ud_handle_t hdl, const char *const argv[])
 {
-	ud_convo_t cno;
+	const char *arg;
 
-	for (int i = 1; i < argc; i++) {
-		/* query the bugger */
-		cno = e123ify_send(hdl, argv[i]);
-		/* receive */
-		(void)e123ify_recv(hdl, cno);
+	while ((arg = *argv++) != NULL) {
+		__e123ify(hdl, arg);
 	}
 	return;
 }
@@ -172,17 +200,13 @@ stdio_mode(ud_handle_t hdl)
 {
 	ssize_t nread;
 	size_t llen = 256;
-	ud_convo_t cno;
 	char *lbuf;
 
 	/* get a line buffer */
 	lbuf = malloc(llen);
 	/* process line by line */
 	while ((nread = getline(&lbuf, &llen, stdin)) > 0) {
-		/* query the bugger */
-		cno = e123ify_send(hdl, lbuf);
-		/* receive */
-		(void)e123ify_recv(hdl, cno);
+		__e123ify(hdl, lbuf);
 	}
 	/* free the line buffer */
 	free(lbuf);
@@ -190,16 +214,56 @@ stdio_mode(ud_handle_t hdl)
 }
 
 
+#if defined HAVE_POPT
+static const struct poptOption const __opts[] = {
+        {"sed-mode", 's', POPT_ARG_NONE,
+	 &sed_mode, 0,
+	 "Print the original and the reformatted string like "
+	 "\"s/original/reformatted/\"", NULL },
+        POPT_AUTOHELP
+        POPT_TABLEEND
+};
+#endif  /* HAVE_POPT */
+
+static const char *const*
+parse_cl(size_t argc, const char *argv[])
+{
+#if defined HAVE_POPT
+        int rc;
+        poptContext opt_ctx;
+
+        opt_ctx = poptGetContext(NULL, argc, argv, __opts, 0);
+        poptSetOtherOptionHelp(
+		opt_ctx,
+		"[reconciliation-options] "
+		"module [module [...]]");
+
+        /* auto-do */
+        while ((rc = poptGetNextOpt(opt_ctx)) > 0) {
+                /* Read all the options ... */
+                ;
+        }
+        return poptGetArgs(opt_ctx);
+#else  /* !HAVE_POPT */
+# error "Get popt or get a life ..."
+#endif  /* HAVE_POPT */
+}
+
+
 int
 main(int argc, const char *argv[])
 {
 	struct ud_handle_s __hdl;
+	const char *const *rest;
 
 	/* obtain us a new handle */
 	init_unserding_handle(&__hdl);
 
-	if (argc > 1) {
-		cli_mode(&__hdl, argc, argv);
+	/* parse cli options */
+	rest = parse_cl(argc, argv);
+
+	if (rest != NULL) {
+		cli_mode(&__hdl, rest);
 	} else {
 		stdio_mode(&__hdl);
 	}

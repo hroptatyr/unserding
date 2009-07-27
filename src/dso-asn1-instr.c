5899d519562b7ed61302b5267a2ae991b14fffa9
Sebastian Freundt <hroptatyr@fresse.org>
2009-07-27 21:55:22 +0000

    Summary: minor, provide initial ASN.1 tests Keywords: asn1cat
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

---
+++ src/dso-asn1-instr.c
@@ -0,0 +1,87 @@
+/*** dso-asn1-instr.c -- instruments in ASN notation
+ *
+ * Copyright (C) 2008, 2009 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <sebastian.freundt@ga-group.nl>
+ *
+ * This file is part of unserding.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+/* our master include */
+#include "unserding.h"
+#include "module.h"
+#include "protocore.h"
+#define UNSERSRV
+#include "unserding-dbg.h"
+
+#include "Instrument.h"
+
+static void *instruments;
+
+static void
+instr_add(job_t j)
+{
+	UD_DEBUG("adding instrument\n");
+	return;
+}
+
+static void
+instr_add_xer(job_t j)
+{
+	UD_DEBUG("getting shit from /tmp/instr.xer\n");
+
+	return;
+}
+
+
+void
+init(void *clo)
+{
+	UD_DEBUG("mod/asn1-inestr: loading ...");
+	/* lodging our bbdb search service */
+	ud_set_service(0x4216, instr_add, NULL);
+	ud_set_service(0x4218, instr_add_xer, NULL);
+	UD_DBGCONT("done\n");
+	return;
+}
+
+
+/* dso-asn1-instr.c */

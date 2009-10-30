a732c5eabd6c5e2651c34311d36ff15caa2cd4a8
Sebastian Freundt <hroptatyr@fresse.org>
2009-10-30 12:30:08 +0000

    Summary: minor, provide preliminary cli tool for the pong service Keywords: pingpong library
    Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

---
+++ src/ud-ping.c
@@ -0,0 +1,73 @@
+/*** ud-ping.c -- ping/pong utility
+ *
+ * Copyright (C) 2009 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <sebastian.freundt@ga-group.nl>
+ *
+ * This file is part of unserding.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+
+#include <stdio.h>
+#include <stdbool.h>
+#define __USE_XOPEN
+#include <time.h>
+#include "unserding.h"
+#include "unserding-nifty.h"
+#include "protocore.h"
+#include "seria.h"
+
+static struct ud_handle_s __hdl;
+static ud_handle_t hdl = &__hdl;
+
+int
+main(int argc, const char *argv[])
+{
+	static char buf[UDPC_PKTLEN];
+	ud_packet_t p = BUF_PACKET(buf);
+	ud_convo_t cno;
+
+	if (argc <= 0) {
+		fprintf(stderr, "Usage: ud-ping\n");
+		exit(1);
+	}
+
+	/* obtain a new handle */
+	init_unserding_handle(hdl, PF_INET6);
+	/* init the seria */
+	cno = ud_send_simple(hdl, 0x0004);
+	/* wait */
+	ud_recv_convo(hdl, &p, 2000, cno);
+	/* and lose the handle again */
+	free_unserding_handle(&__hdl);
+	return 0;
+}
+
+/* ud-tick.c ends here */

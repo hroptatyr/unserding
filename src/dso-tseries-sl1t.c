2d02d71417201133c4123cbbe4b0994c658200b8
Sebastian Freundt <hroptatyr@fresse.org>
2009-12-10 23:59:43 +0000

    Summary: minor, make the sl1t reader inspect the trng table Keywords: sl1t Signed-off-by: Sebastian Freundt <hroptatyr@fresse.org>

--- src/dso-tseries-sl1t.c
+++ src/dso-tseries-sl1t.c
@@ -74,41 +74,108 @@ struct tssl1t_s {
 };
 
 
+static inline size_t
+pr_ts(char *restrict buf, uint32_t sec)
+{
+	struct tm tm[1];
+	gmtime_r((time_t*)&sec, tm);
+	strftime(buf, 32, "%Y-%m-%d %H:%M:%S", tm);
+	return 19;
+}
+
+static void
+print_trng(uint16_t idx, void *clo)
+{
+	char tsf[32], tsl[32];
+	sl1t_fhdr_t fhdr = sl1t_fio_fhdr(clo);
+	time_range_t tr;
+
+	if (sl1t_fhdr_trngtbl_p(fhdr) &&
+	    (tr = sl1t_fhdr_nth_trng(fhdr, idx)) != NULL) {
+		pr_ts(tsf, tr->lo);
+		pr_ts(tsl, tr->hi);
+		fputc(' ', stdout);
+		fputs(tsf, stdout);
+		fputc(' ', stdout);
+		fputs(tsl, stdout);
+	}
+	return;
+}
+
+static void
+fill_urn_sym(uint16_t idx, const char *sym, void *UNUSED(clo))
+{
+	if (sl1t_stbl_sym_slot_free_p(sym)) {
+		return;
+	}
+	fprintf(stdout, "%hu: %s", idx, sym);
+
+	/* print the life span of this instrument in this file */
+	print_trng(idx, clo);
+	fputc('\n', stdout);
+	return;
+}
+
+static void
+fill_urn_sec(uint16_t idx, su_secu_t sec, void *clo)
+{
+	if (sl1t_stbl_sec_slot_free_p(sec)) {
+		return;
+	} else {
+		uint32_t quodi = su_secu_quodi(sec);
+		int32_t quoti = su_secu_quoti(sec);
+		uint16_t pot = su_secu_pot(sec);
+		fprintf(stdout, "%hu: %u/%i@%hu", idx, quodi, quoti, pot);
+	}
+
+	/* print the life span of this instrument in this file */
+	print_trng(idx, clo);
+	fputc('\n', stdout);
+	return;
+}
+
+static void
+fill_urns(tssl1t_t ctx)
+{
+	void *rdr = ctx->rdr;
+	sl1t_fio_trav_stbl(rdr, fill_urn_sec, fill_urn_sym, rdr);
+	return;
+}
+
+
 static const char fxsl1tf[] = "/home/freundt/.unserding/EUR_hist.sl1t";
+static struct tssl1t_s my_ctx[1];
 
 static void
 load_sl1t_file(tssl1t_t ctx)
 {
+	UD_DEBUG("opening sl1t (%s) ...", fxsl1tf);
 	if ((ctx->infd = open(fxsl1tf, O_RDONLY, 0644)) < 0) {
 		/* file not found */
 		ctx->rdr = NULL;
+		UD_DBGCONT("failed\n");
 		return;
 	}
 	ctx->rdr = make_sl1t_reader(ctx->infd);
+	UD_DBGCONT("done\n");
 	return;
 }
 
 void
-fetch_urn_file(void)
+fetch_urn_sl1t(void)
 {
 /* make me thread-safe and declare me */
-	if (conn == NULL) {
+	if (my_ctx->rdr == NULL) {
 		return;
 	}
 
-	UD_DEBUG("leeching overview ...");
-	uddb_qry(conn, ovqry, sizeof(ovqry)-1, ovqry_rowf, NULL);
-	UD_DBGCONT("done\n");
-
-	UD_DEBUG("inspecting URNs ...");
-	fill_urns();
+	UD_DEBUG("inspecting sl1t ...");
+	fill_urns(my_ctx);
 	UD_DBGCONT("done\n");
 	return;
 }
 
 
-static struct tssl1t_s my_ctx[1];
-
 void
 dso_tseries_sl1t_LTX_init(void *UNUSED(clo))
 {

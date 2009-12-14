b4ea581201283d675a57962b3e81144d5337beba
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2009-12-14 14:53:15 +0000

    Summary: minor, migrate to sl1t ticks, part IV Keywords: seria secu uterus
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- cli/ud-snap.c
+++ cli/ud-snap.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <pfack/uterus.h>
 #include "unserding.h"
 #include "unserding-nifty.h"
 #include "protocore.h"
 #include "tseries.h"
+#include <sushi/m30.h>
 
 static struct ud_handle_s __hdl;
 static ud_handle_t hdl = &__hdl;
 static bool xmlp;
 
 static void
-t_cb(sl1tick_t t, void *UNUSED(clo))
+t_cb(su_secu_t s, scom_t th, void *UNUSED(clo))
 {
-	uint32_t i = sl1tick_instr(t);
-	int tt = (int)sl1tick_tick_type(t);
-	long int s = (long int)sl1tick_timestamp(t);
-	short int ms = (short int)sl1tick_msec(t);
-	double v = ffff_monetary32_d(sl1tick_value(t));
+	const_sl1t_t t = (const void*)th;
+	uint32_t qd = su_secu_quodi(s);
+	int32_t qt = su_secu_quoti(s);
+	uint16_t p = su_secu_pot(s);
+	uint16_t ttf = scom_thdr_ttf(th);
+	time_t ts = scom_thdr_sec(th);
+	uint16_t ms = scom_thdr_msec(th);
+	double v = ffff_m30_d(ffff_m30_get_ui32(t->v[0]));
 
-	fprintf(stdout, "ii:%u  tt:%d ts:%ld.%03hd v:%2.4f\n", i, tt, s, ms, v);
+	fprintf(stdout, "ii:%u/%i@%hu  tt:%d ts:%ld.%03hd v:%2.4f\n",
+		qd, qt, p, ttf, ts, ms, v);
 	return;
 }
 
@@ -30,7 +34,7 @@ main(int argc, const char *argv[])
 	su_secu_t cid[argc];
 	int n = 0;
 	time_t ts = time(NULL);
-	uint32_t bs = PFTB_EOD | PFTB_STL;
+	uint32_t bs = ~0;
 
 	for (int i = 1; i < argc; i++) {
 		uint32_t qd = strtol(argv[i], NULL, 10);

1ec78fdab7a8ce0d19d7bec01dd8b265cb41e86f
Sebastian Freundt <sebastian.freundt@ga-group.nl>
2010-01-05 16:53:30 +0000

    Summary: minor, start supporting ute files using blisters Keywords: tseries uterus
    Signed-off-by: Sebastian Freundt <sebastian.freundt@ga-group.nl>

--- src/dso-tseries-ute.c
+++ src/dso-tseries-ute.c
@@ -1,4 +1,4 @@
-/*** tseries.h -- stuff that is soon to be replaced by ffff's tseries
+/*** dso-tseries-ute.c -- ute tick files
  *
  * Copyright (C) 2009 Sebastian Freundt
  *
@@ -35,51 +35,71 @@
  *
  ***/
 
-#if !defined INCLUDED_tseries_private_h_
-#define INCLUDED_tseries_private_h_
+#if defined HAVE_CONFIG_H
+# include "config.h"
+#endif	/* HAVE_CONFIG_H */
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <pthread.h>
 
-#include "tseries.h"
-#include "tscoll.h"
-#include "tscache.h"
-
-/* the main one used in dso-tseries.c */
-extern tscache_t tscache;
-
-#if defined HAVE_MYSQL
-extern void
-fetch_urn_mysql(void);
-#endif	/* HAVE_MYSQL */
-extern void fetch_urn_sl1t(void);
-extern void fetch_urn_ute(void);
-
-
-/* module like helpers */
-extern void dso_tseries_LTX_init(void*);
-extern void dso_tseries_LTX_deinit(void*);
-extern void dso_tseries_mysql_LTX_init(void*);
-extern void dso_tseries_mysql_LTX_deinit(void*);
-extern void dso_tseries_frobq_LTX_init(void*);
-extern void dso_tseries_frobq_LTX_deinit(void*);
-extern void dso_tseries_sl1t_LTX_init(void*);
-extern void dso_tseries_sl1t_LTX_deinit(void*);
+#include <sushi/sl1t.h>
+#include <sushi/sl1tfile.h>
+/* our master include */
+#include "unserding.h"
+#include "module.h"
+#include "protocore.h"
+#define UNSERSRV
+#include "unserding-dbg.h"
+#include "unserding-ctx.h"
+#include "unserding-private.h"
+/* for higher level packet handling */
+#include "seria-proto-glue.h"
+#include "tseries-private.h"
 
 
-/* frob queue mumbo jumbo */
-extern void defer_frob(tseries_t tser, dse16_t refds, bool immediatep);
-extern void frobnicate(void);
+/* ugly */
+#include "uteseries.c"
 
 
-static inline uint8_t
-index_in_pkt(time_t ts)
+static void *my_ctx;
+
+void
+fetch_urn_ute(void)
 {
-/* find the index of the date encoded in dse inside a tick bouquet */
-	dse16_t dse = time_to_dse(ts);
-	dse16_t anchor = time_to_dse(442972800);
-	uint8_t res = (dse - anchor) % 14;
-	static uint8_t offs[14] = {
-		-1, 0, 1, 2, 3, 4, -1, -1, 5, 6, 7, 8, 9, -1
-	};
-	return offs[res];
+/* make me thread-safe and declare me */
+	if (my_ctx == NULL) {
+		return;
+	}
+
+	UD_DEBUG("inspecting sl1t ...");
+	fill_urns(my_ctx);
+	UD_DBGCONT("done\n");
+	return;
 }
 
-#endif	/* INCLUDED_tseries_private_h_ */
+void
+dso_tseries_ute_LTX_init(void *UNUSED(clo))
+{
+	UD_DEBUG("mod/tseries-ute: loading ...");
+	my_ctx = open_ute_file(my_hardcoded_file);
+	UD_DBGCONT("done\n");
+	return;
+}
+
+void
+dso_tseries_ute_LTX_deinit(void *UNUSED(clo))
+{
+	UD_DEBUG("mod/tseries-ute: unloading ...");
+	if (my_ctx != NULL) {
+		close_ute_file(my_ctx);
+	}
+	UD_DBGCONT("done\n");
+	return;
+}
+
+/* dso-tseries-ute.c ends here */

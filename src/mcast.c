4a23089614af826d70afe63ab4b7e56f6218629d
Sebastian Freundt <freundt@ga-group.nl>
2012-05-22 15:18:00 +0000

    minor, make mcast6 listeners listen on all ports
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/mcast.c
+++ src/mcast.c
@@ -212,7 +212,8 @@ mcast6_listener_init(int s, short unsigned int port)
 	};
 
 	__sa6.sa6.sin6_family = AF_INET6;
-	__sa6.sa6.sin6_port = htons(port);
+	/* listen on all ports */
+	__sa6.sa6.sin6_port = 0;
 
 	/* allow many many many servers on that port */
 	__reuse_sock(s);
